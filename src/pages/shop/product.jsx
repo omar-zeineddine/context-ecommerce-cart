53d04d13ffb7cad067e2512c2a1f09f06f00b3fe
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-12 20:14:10 +0200

    feat - add product component

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  return (
+    <div className="product">
+      <img src={productImage} alt="" />
+      <div className="description">
+        <p>
+          <strong>{productName}</strong>
+        </p>
+        <p>${price}</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,3 +1,17 @@
+import { products } from "../../data/products";
+import { Product } from "./product";
+
 export const Shop = () => {
-  return <div>shop</div>;
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>A&N shop</h1>
+      </div>
+      <div className="products">
+        {products.map((product, id) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
