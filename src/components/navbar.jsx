67dc657f6dcffa504b50e266ed37403d8adb11c2
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-12 19:06:13 +0200

    feat - add navbar item

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
 
 export const Navbar = () => {
-  return <div>N</div>;
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+    </div>
+  );
 };
