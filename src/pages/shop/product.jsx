423a0eb336ec6f33e7fcad5bd92ed2cf200df03a
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-14 12:14:48 +0200

    feat - display cart item amount conditionally

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -3,7 +3,9 @@ import { ShopContext } from "../../context/shop-context";
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
-  const { addToCart } = useContext(ShopContext);
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const cartItemAmount = cartItems[id];
+
   return (
     <div className="product">
       <img src={productImage} alt="" />
@@ -12,7 +14,9 @@ export const Product = (props) => {
           <strong>{productName}</strong>
         </p>
         <p>${price}</p>
-        <button className="addToCartBtn">Add to Cart</button>
+        <button className="addToCartBtn" onClick={() => addToCart(id)}>
+          Add to Cart {cartItemAmount > 0 ? <>({cartItemAmount})</> : null}
+        </button>
       </div>
     </div>
   );
