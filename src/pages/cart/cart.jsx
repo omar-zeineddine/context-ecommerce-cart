73dc8b72334182f2cf6d2330f738e500ba29bf40
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-14 12:21:48 +0200

    feat - add cart item component

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,3 +1,22 @@
+import { useContext } from "react";
+import { products } from "../../data/products";
+import { ShopContext } from "../../context/shop-context";
+import { CartItem } from "./cart-item";
+
 export const Cart = () => {
-  return <div>cart</div>;
+  const { cartItems } = useContext(ShopContext);
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your cart items</h1>
+      </div>
+      <div className="cartItems">
+        {products.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+    </div>
+  );
 };
