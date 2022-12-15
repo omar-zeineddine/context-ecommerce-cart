0cdfeb40917d0f754a2c777f31867496a55893bb
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-15 10:36:03 +0200

    style - add cart and cart items styles

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,8 +1,9 @@
+import "./cart.css";
 export const CartItem = (props) => {
   const { id, productName, price, productImage } = props.data;
   return (
     <div className="cartItem">
-      <img src={productImage} alt="" />
+      <img src={productImage} />
       <div className="description">
         <p>
           <b>{productName}</b>
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,15 +2,16 @@ import { useContext } from "react";
 import { products } from "../../data/products";
 import { ShopContext } from "../../context/shop-context";
 import { CartItem } from "./cart-item";
+import "./cart.css";
 
 export const Cart = () => {
   const { cartItems } = useContext(ShopContext);
   return (
     <div className="cart">
       <div>
-        <h1>Your cart items</h1>
+        <h1>Your Cart Items</h1>
       </div>
-      <div className="cartItems">
+      <div className="cart">
         {products.map((product) => {
           if (cartItems[product.id] !== 0) {
             return <CartItem data={product} />;
