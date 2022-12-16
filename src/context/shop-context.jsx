3c2b4455ed25a8ef546f74b5f506548b318e4a53
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-16 23:00:47 +0200

    feat - get cart product totals

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -25,11 +25,23 @@ export const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
   };
 
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = products.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] + itemInfo.price;
+      }
+    }
+    return totalAmount;
+  };
+
   const contextValue = {
     cartItems,
     addToCart,
     removeFromCart,
     updateCartItemCount,
+    getTotalCartAmount,
   };
 
   // console.log(cartItems);
